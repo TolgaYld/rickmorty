034f5e5b94a4b9f6740841b5ef383bd1d506b1d0
Tolga Yildirim <tolga@ty-software.dev>
2026-01-24 22:56:34 +0100

    unused constant

--- core/lib/rickmorty_constants.dart
+++ core/lib/rickmorty_constants.dart
@@ -1,3 +1,2 @@
 export 'src/constants/uri.dart';
-export 'src/constants/theme_mode_keys.dart';
 export 'src/constants/storage_keys.dart';
--- core/lib/src/constants/theme_mode_keys.dart
+++
@@ -1,6 +0,0 @@
-class ThemeModeKeys {
-  const ThemeModeKeys._();
-
-  static const String cachedThemeMode = 'CACHED_THEME_MODE';
-  static const String cachedUseSystemTheme = 'CHACHED_USE_SYSTEM_THEME';
-}
